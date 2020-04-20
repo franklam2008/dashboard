e81c2261993c03ab740502b9e6637bc85fb2ffbb
franklam2008 <franklam2008@gmail.com>
2020-04-20 02:14:03 -0400

    contact page

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  myForm: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.myForm = this.fb.group({
+      email: '',
+      message: '',
+      career: '',
+    });
+  }
+
+  test(){
+    console.log(this.myForm.value);
   }
 
 }
