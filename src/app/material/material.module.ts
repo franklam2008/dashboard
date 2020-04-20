85d8c02f1759d279365f5132e2c1cb83b5eb2a23
franklam2008 <franklam2008@gmail.com>
2020-04-20 02:14:38 -0400

    material module

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -4,11 +4,24 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatCardModule } from '@angular/material/card';
 import { MatExpansionModule } from '@angular/material/expansion'; import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon'; import { MatTabsModule } from '@angular/material/tabs';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDividerModule } from '@angular/material/divider';
 const MaterialComponents = [
   MatButtonModule,
-  MatSliderModule, MatExpansionModule, MatToolbarModule,
-  MatCardModule, MatIconModule, MatTabsModule,
+  MatSliderModule,
+  MatExpansionModule,
+  MatToolbarModule,
+  MatCardModule,
+  MatIconModule,
+  MatTabsModule,
+  MatTableModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatDividerModule,
 ];
 
 @NgModule({
