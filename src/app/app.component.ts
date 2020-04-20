80455c67cb561d72a240b83cd0dfa86447bfc8ee
franklam2008 <franklam2008@gmail.com>
2020-04-20 02:15:47 -0400

    remove use to home component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { User } from './model/user/user.model';
+import { DataService } from './data.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  users$: User[];
   title = 'dashboard';
+  constructor(private dataService: DataService) { }
+  ngOnInit() {
+    return this.dataService.getUsers()
+      .subscribe(data =>
+        this.users$ = data
+      );
+  }
+  test(){
+    console.log(this.users$);
+  }
 }
