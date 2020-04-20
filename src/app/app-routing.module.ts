26c11526a0ac161326ba32d68103e8cb014efafc
franklam2008 <franklam2008@gmail.com>
2020-04-20 02:13:46 -0400

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { TicTacToeComponent } from './pages/tic-tac-toe/tic-tac-toe.component';
+import { ClientListComponent } from './pages/client-list/client-list.component';
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
+  { path: 'tic-tac-toe', component: TicTacToeComponent },
+  { path: 'client-list', component: ClientListComponent },
 ];
 
 @NgModule({
