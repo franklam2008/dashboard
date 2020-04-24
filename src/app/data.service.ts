4346e3cd46085a7ea9d4400b807954ccc0c8282e
franklam2008 <franklam2008@gmail.com>
2020-04-24 19:49:02 -0400

    update service

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,5 +1,3 @@
-
-
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from './model/user/user.model';
@@ -11,7 +9,13 @@ export class DataService {
   jsonplaceUrl = 'https://jsonplaceholder.typicode.com/users';
   constructor(private http: HttpClient) { }
 
+  chartData: number[] = [85, 72, 78, 75, 77, 75];
+
   getUsers() {
     return this.http.get<User[]>(this.jsonplaceUrl);
+  }
+
+  getChartData() {
+    return this.chartData;
   }
 }
