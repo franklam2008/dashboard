6d0356a518b29704d50b3c43bd9c5e08271fa8b3
franklam2008 <franklam2008@gmail.com>
2020-04-24 19:49:11 -0400

    test chart

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartDataSets, ChartOptions } from 'chart.js';
+import { Color, Label } from 'ng2-charts';
+import { DataService } from 'src/app/data.service';
 
 @Component({
   selector: 'app-about',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
+  lineChartData: ChartDataSets[] = [
+    { data: this.dataService.getChartData(), label: 'Crude oil prices' },
+  ];
 
-  constructor() { }
+  lineChartLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June'];
+
+  lineChartOptions = {
+    responsive: true,
+  };
+
+  lineChartColors: Color[] = [
+    {
+      borderColor: 'black',
+      backgroundColor: 'rgba(255,255,0,0.28)',
+    },
+  ];
+
+  lineChartLegend = true;
+  lineChartPlugins = [];
+  lineChartType = 'line';
+  constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
+    console.log(this.dataService.getChartData());
+
   }
 
 }
